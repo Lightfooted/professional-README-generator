c02dc13279f9ed2f991d383a62b421bedf06c702
Lightfooted <vesselofbalance@hotmail.com>
2021-08-14 15:56:16 -0400

    work in progress, added descript and link

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -13,7 +13,12 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  
+  ## Description:
+  ${data.description}
 
+  ## Link:
+  ${data.link}
 `;
 }
 
